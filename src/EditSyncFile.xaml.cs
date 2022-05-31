1e6801065b81be2264f890cabfe7f395f20cf543
OSN-DEV <osndev1973@gmail.com>
2022-05-31 21:11:18 +0900

    テキストボックスへのドラッグ＆ドロップに対応

--- src/EditSyncFile.xaml.cs
+++ src/EditSyncFile.xaml.cs
@@ -30,9 +30,32 @@ namespace MySynchFiles {
         #endregion
 
         #region Event
+        private void CustomTextBox_PreviewDragOver(object sender, DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                e.Effects = DragDropEffects.Copy;
+                e.Handled = true;
+            }
+        }
+        private void CustomTextBox_Drop(object sender, DragEventArgs e) {
+            if (!(e.Data.GetData(DataFormats.FileDrop) is string[] files)) {
+                return;
+            }
+            var txt = sender as TextBox;
+            var model = (SyncFileDataModel)this.DataContext;
+
+            txt.Text = files[0];
+            if (txt.Tag.ToString() == "Local") {
+                model.LocalFile = files[0];
+            } else {
+                model.ServerFile = files[0];
+            }
+
+            ((TextBox)sender).Text = files[0];
+        }
         public void OK_Click(object sender, EventArgs e) {
             this.DialogResult = true;
         }
         #endregion
+
     }
 }
